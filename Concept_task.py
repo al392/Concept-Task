0be1de63d113278d87d1a83bee435dd213c43522
al392 <76264163+al392@users.noreply.github.com>
2021-01-04 18:18:50 +0200

    Concept Task Part2

---
+++ Concept_task.py
@@ -0,0 +1,32 @@
+#Sliding using the Keyboard
+elif event.type == KEYUP:
+                
+                if event.key in (K_LEFT, K_a) and isValidMove(mainBoard, LEFT):
+                    slideTo = LEFT
+                elif event.key in (K_RIGHT, K_d) and isValidMove(mainBoard, RIGHT):
+                    slideTo = RIGHT
+                elif event.key in (K_UP, K_w) and isValidMove(mainBoard, UP):
+                    slideTo = UP
+                elif event.key in (K_DOWN, K_s) and isValidMove(mainBoard, DOWN):
+                    slideTo = DOWN
+
+        if slideTo:
+            slideAnimation(mainBoard, slideTo, 'Click tile or press arrow keys to slide.', 8) 
+            makeMove(mainBoard, slideTo)
+            allMoves.append(slideTo) 
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+#Terminate the Game
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def checkForQuit():
+    for event in pygame.event.get(QUIT): 
+        terminate() 
+    for event in pygame.event.get(KEYUP): 
+        if event.key == K_ESCAPE:
+            terminate() 
+        pygame.event.post(event) 
